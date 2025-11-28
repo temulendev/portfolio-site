1372d0c1f8aa19028f27adeee0df36c43557b7ff
Temulen Iveelt <temuleniveelt@Mac.fios-router.home>
2025-11-28 01:02:45 -0500

    v1 of site done

---
+++ script.js
@@ -0,0 +1,29 @@
+const nameElement = document.getElementById('name');
+const fullName = "Temulen Iveelt"; // Updated to match your name
+const internElement = document.getElementById('internships');
+const fullInternTxt = "[ Open to Summer 2026 Internships ]";
+let i = 0;
+let j=0;
+
+nameElement.innerText = "whoami? "; // Clear it initially
+internElement.innerText = "";
+
+function typeWriter() {
+    if (i < fullName.length) {
+        nameElement.innerText += fullName.charAt(i);
+        i++;
+        setTimeout(typeWriter, 150); // Speed of typing
+    }
+}
+
+function typeWriter2() {
+    if (j < fullInternTxt.length) {
+        internElement.innerText += fullInternTxt.charAt(j);
+        j++;
+        setTimeout(typeWriter2, 70); // Speed of typing
+    }
+}
+
+// Start typing after a slight delay
+setTimeout(typeWriter, 500);
+setTimeout(typeWriter2, 600);
